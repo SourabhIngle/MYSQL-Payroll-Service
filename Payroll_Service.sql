ed54857301be80fcbc371f81a1920b8d727f7878
SourabhIngle <100rabhingle@gmail.com>
2023-05-31 14:47:22 +0530

    Create Table Employee Payroll

--- Payroll_Service.sql
+++ Payroll_Service.sql
@@ -22,3 +22,43 @@ Database changed
 
 --------------------END OF UC1---------------------------
 
+-------------------------UC2-----------------------------
+---------CREATE TABLE EMPLOYEE_PAYROLL-----------
+mysql> use payroll_service;
+Database changed
+
+mysql> EmpID BIGINT(4) PRIMARY KEY auto_increment,
+    -> Name VARCHAR(15),
+    -> Salary BIGINT(15),
+    -> Start_Date DATE
+    -> );
+
+    mysql> create table Employee_Payroll(
+        -> EmpID BIGINT(4) PRIMARY KEY auto_increment,
+        -> Name VARCHAR(15),
+        -> Salary BIGINT(15),
+        -> Start_Date DATE
+        -> );
+    Query OK, 0 rows affected, 2 warnings (0.05 sec)
+
+    mysql> show tables;
+    +---------------------------+
+    | Tables_in_payroll_service |
+    +---------------------------+
+    | employee_payroll          |
+    +---------------------------+
+    1 row in set (0.01 sec)
+
+    mysql> DESC Employee_Payroll;
+    +------------+-------------+------+-----+---------+----------------+
+    | Field      | Type        | Null | Key | Default | Extra          |
+    +------------+-------------+------+-----+---------+----------------+
+    | EmpID      | bigint      | NO   | PRI | NULL    | auto_increment |
+    | Name       | varchar(15) | YES  |     | NULL    |                |
+    | Salary     | bigint      | YES  |     | NULL    |                |
+    | Start_Date | date        | YES  |     | NULL    |                |
+    +------------+-------------+------+-----+---------+----------------+
+    4 rows in set (0.00 sec)
+
+--------------------END OF UC2---------------------------
+
